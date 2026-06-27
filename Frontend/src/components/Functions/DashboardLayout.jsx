c75c74c3fe79d8a6dd6e8274039710dc1df22ae0
Bornasal <allen.bornasal@benilde.edu.ph>
2026-06-28 00:22:23 +0800

    AND ANOTHER ONE

--- Frontend/src/components/Functions/DashboardLayout.jsx
+++ Frontend/src/components/Functions/DashboardLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router'
 import Navbar from '../.Navbar/Navbar'
-import { useAuth } from '../../.context/AuthContext'
+import { useAuth } from "../../.Context/AuthContext";
 
 export default function DashboardLayout() {
   const { auth } = useAuth();
